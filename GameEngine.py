cdfa9ae5aaa0a32cb3dd41f5bf15cc3780026552
kmfrost <1356785+kmfrost@users.noreply.github.com>
2020-07-05 13:00:14 -0600

    First commit of the initial classes and functions.

---
+++ GameEngine.py
@@ -0,0 +1,29 @@
+
+class GameEngine:
+    def __init__(self):
+        pass
+
+    def reset(self):
+        pass
+    def get_data(self):
+        pass
+        
+    def get_last_action(self):
+        pass
+    
+    def do_action(self):
+        pass
+    
+    def _do_take_camels(self):
+        pass
+    
+    def _do_sell_cards(self):
+        pass
+    
+    def _do_grab_card(self):
+        pass
+    
+    def _do_trade_cards(self):
+        pass
+    
+    
---
+++ Player.py
@@ -0,0 +1,9 @@
+class Player:
+    def __init__(self):
+        pass
+    
+    def valid_actions(self):
+        pass
+    
+    def take_action(self):
+        pass
